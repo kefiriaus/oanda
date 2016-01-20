4fb49163870b666c7fce4be3a07a4ce29958c06c
Sergey Golovkin <me@golovkin.cc>
2016-01-20 23:56:32 +0300

    updated with portfolio

--- trading.rb
+++ trading.rb
@@ -1,19 +1,22 @@
 require 'thread'
 
 require_relative 'execution'
+require_relative 'portfolio'
 require_relative 'settings'
 require_relative 'strategy'
 require_relative 'streaming'
 
-def trade(events, strategy, execution)
+def trade(events, strategy, portfolio, execution)
     loop do
         event = events.pop(false)
         puts event.type
         unless event.nil?
-            if event.type == "TICK"
+            if event.type == 'TICK'
                 strategy.calculate_signals(event)
-            elsif event.type == "ORDER"
-                puts "Executing order!"
+            elsif event.type == 'SIGNAL'
+                portfolio.execute_signal(event)
+            elsif event.type == 'ORDER'
+                puts 'Executing order!'
                 execution.execute_order(event)
             end
         end
@@ -24,20 +27,18 @@ end
 
 events = Queue.new
 instrument = 'EUR_USD'
-units = 1000
+equity = 100000.0
 
 prices = StreamingForexPrices.new(STREAM_DOMAIN, ACCESS_TOKEN, ACCOUNT_ID, instrument, events)
 
+strategy = TestStrategy(instrument, events)
+
+portfolio = Portfolio(prices, events, equity=equity)
+
 execution = Execution.new(API_DOMAIN, ACCESS_TOKEN, ACCOUNT_ID)
 
-strategy = TestRandomStrategy.new(instrument, units, events)
-
-puts 'test'
-
-trade_thread = Thread.new { trade(events, strategy, execution) }
+trade_thread = Thread.new { trade(events, strategy, portfolio, execution) }
 price_thread = Thread.new { prices.stream_to_queue }
-
-#prices.stream_to_queu
 
 trade_thread.join
 price_thread.join
